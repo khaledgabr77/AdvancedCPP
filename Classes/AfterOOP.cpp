bd222f56e36f7ed4e081881e385ae407d314f5c9
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-18 17:17:52 +0200

    polymorphism

--- Classes/AfterOOP.cpp
+++ Classes/AfterOOP.cpp
@@ -1,57 +1,49 @@
 #include <iostream>
+class Pizza {
+protected:
+  void CreateDough() { std::cout << "Create Dough" << std::endl; }
+  void Bake() { std::cout << "Baking" << std::endl; }
+  virtual void AddToppings() = 0;
 
-class ChikenPizza {
 public:
-  void CreateDough() { std::cout << "Create Dough" << std::endl; }
+  void MakePizza() {
+    CreateDough();
+    Bake();
+    AddToppings();
+    std::cout << "Pizza is Ready" << std::endl;
+  }
+};
+
+class ChikenPizza : public Pizza {
+
   void AddToppings() { std::cout << "Adding Chicken Toppings" << std::endl; }
-  void Bake() { std::cout << "Baking" << std::endl; }
-  void MakePizza() {
-    CreateDough();
-    AddToppings();
-    Bake();
-    std::cout << "Pizza is Ready" << std::endl;
-  }
 };
-class BeefPizza {
-public:
-  void CreateDough() { std::cout << "Create Dough" << std::endl; }
+class BeefPizza : public Pizza {
   void AddToppings() { std::cout << "Adding Beef Toppings" << std::endl; }
-  void Bake() { std::cout << "Baking" << std::endl; }
-  void Make() {}
-  void MakePizza() {
-    CreateDough();
-    AddToppings();
-    Bake();
-    std::cout << "Pizza is Ready" << std::endl;
-  }
 };
-class CheesePizza {
-public:
-  void CreateDough() { std::cout << "Create Dough" << std::endl; }
+class CheesePizza : public Pizza {
   void AddToppings() { std::cout << "Adding Cheese Toppings" << std::endl; }
-  void Bake() { std::cout << "Baking" << std::endl; }
-  void MakePizza() {
-    CreateDough();
-    AddToppings();
-    Bake();
-    std::cout << "Pizza is Ready" << std::endl;
-  }
+};
+void ll(Pizza *p) { p->MakePizza(); }
+class Shrimp : public Pizza {
+  void AddToppings() { std::cout << "Adding Shrimp Topping" << std::endl; }
 };
 int main() {
-  ChikenPizza chicken;
-  BeefPizza beef;
-  CheesePizza cheese;
-  std::cout << "For checkin Enter 1, for beef Enter 2, for cheese, Enter 3"
+
+  std::cout << "For checkin Enter 1, for beef Enter 2, for cheese Enter 3, for "
+               "shrimp Enter 4"
             << std::endl;
   int choise;
   std::cin >> choise;
 
   if (choise == 1) {
-    chicken.MakePizza();
+    ll(new ChikenPizza());
   } else if (choise == 2) {
-    beef.MakePizza();
+    ll(new BeefPizza());
   } else if (choise == 3) {
-    cheese.MakePizza();
+    ll(new CheesePizza());
+  } else if (choise == 4) {
+    ll(new Shrimp());
   } else {
     std::cerr << "Error" << std::endl;
   }
