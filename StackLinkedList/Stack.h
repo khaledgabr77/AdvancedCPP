1d38072859a213cf08b863763bb2dc312c601675
khaledgabr77 <khaledgabr77@gmail.com>
2021-03-10 17:11:43 +0200

    LinkedList Implemntation

---
+++ StackLinkedList/Stack.h
@@ -0,0 +1,31 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef char StackEntry;
+
+#define IMPLEMENTATION
+#ifdef IMPLEMENTATION
+
+typedef struct stacknode {
+  StackEntry entry;
+  struct stacknode *next;
+
+} StackNode;
+
+typedef struct stack {
+  StackNode *top;
+
+} Stack;
+
+#endif // IMPLEMENTATION
+
+void CreateStack(Stack *);
+void Push(Stack *, StackEntry *);
+void Pop(Stack *, StackEntry *);
+int StackFull(Stack *);
+int StackEmpty(Stack *);
+void ClearStack(Stack *);
+void TraverseStack(Stack *, void (*Display)(StackEntry));
+void Display(Stack *);
+
+#endif
